0af335e60fd158197f10fdb805c225987c35f23a
ssum1ra <101113206+ssum1ra@users.noreply.github.com>
2023-04-01 22:12:34 +0900

    random-search 2 commit

--- Random-Search/random_search.py
+++ Random-Search/random_search.py
@@ -2,9 +2,8 @@ import numpy as np
 import csv
 import random
 
+
 # Euclidean distance measuring function
-
-
 def distance(x, y):
     dist = np.linalg.norm(np.array(x)-np.array(y))
     return dist
