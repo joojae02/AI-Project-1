f9b73e7c100bf4cbc7cdec86a38d3d42e38f9899
byeoneunyoung <ebyeon147@soongsil.ac.kr>
2023-04-02 21:55:40 +0900

    number2_eunyoung3

--- Number2.py
+++ Number2.py
@@ -6,12 +6,12 @@ import csv
 # given cities
 cities = []
 
+populationSize = 60  # 염색체 개수
+s = 1000  # 염색체 하나의 유전자 개수
+mutationRate = 0.2  # 돌연 변이 확률
+targetValue = 20000
+bestValue = 50000
 
-populationSize = 60
-s = 1000
-mutationRate = 0.5
-targetValue = 40000
-bestValue = 40000
 
 # Euclidean distance measuring function
 def distance(x, y):
@@ -25,7 +25,7 @@ class Chromosome:
         self.genes = g
         self.fitness = 0
         if self.genes.__len__ () == 0:
-            temp_list = list(range(1, s))
+            temp_list = list (range (1, 1000))
             random.shuffle (temp_list)
             # ordering data set randomly
             self.genes = temp_list.copy ()
@@ -34,7 +34,6 @@ class Chromosome:
 
         self.fitness = 0
         value = 0
-        global bestValue
 
         for i in range (len (self.genes)):
             pos_city_1 = cities[self.genes[i]]
@@ -44,17 +43,13 @@ class Chromosome:
             # accumulation
             value += dist
 
-        self.fitness = ((targetValue - value) + (targetValue - bestValue)) / 3
-        if self.fitness < bestValue:
-            bestValue = self.fitness
+        self.fitness = value
         return self.fitness
 
+
 def print_p(pop):
-    i = 0
-    for x in pop:
-        print("염색체 #", i, "=", x, "적합도=", x.fitness)
-        i += 1
-    print("")
+    print (pop[0].genes[:10], "적합도=", pop[0].fitness)
+    print ("")
 
 
 # 선택 연산
@@ -93,17 +88,16 @@ def crossover(pop):
 def mutation(c):
     if random.random () < mutationRate:
         # 0.0 <= ~ < 1.0 실수를 반환
-        x, y = random.sample (list (range (1, 1000)), 2)
+        x, y = random.sample (list (range (0, 999)), 2)
         c.genes[y], c.genes[x] = c.genes[x], c.genes[y]
 
 
 # 1. get solution sequence and reordering (sort from 0)
 with open ('TSP.csv', mode='r', newline='', encoding='utf-8-sig') as tsp:
-    reader = csv.reader(tsp)
-    for row in reader :
-        tmp = [float(row[0]), float(row[1])]
-        cities.append(tmp)
-
+    reader = csv.reader (tsp)
+    for row in reader:
+        tmp = [float (row[0]), float (row[1])]
+        cities.append (tmp)
 
 population = []
 i = 0
@@ -114,17 +108,16 @@ while i < populationSize:
     i += 1
 
 count = 0
-population.sort (key=lambda x: x.cal_fitness (), reverse=True)
-print ("generation number: ", count)
+population.sort (key=lambda x: x.cal_fitness ())
 count = 1
 
-max_fitness = targetValue
+max_fitness = 0
 
-print(population[0].genes)
-print(population[0].fitness)
+print (population[0].genes[:10])
+print (population[0].fitness)
 
-while population[0].fitness < targetValue:
-    if population[0].fitness < max_fitness:
+while population[0].fitness > targetValue:
+    if population[0].fitness > max_fitness:
         mutationRate = mutationRate * 0.9
         max_fitness = population[0].fitness
     new_pop = []
@@ -141,9 +134,18 @@ while population[0].fitness < targetValue:
     for c in population:
         mutation (c)
 
-    population.sort (key=lambda x: x.cal_fitness (), reverse=True)
+    population.sort (key=lambda x: x.cal_fitness ())
+
+    if population[0].fitness < bestValue:
+        bestValue = population[0].fitness
     fitness_list.append (population[0].fitness)
-    print ("세대 번호=", count)
-    print_p (population)
+
+    print("\nbestValue: ", bestValue)
+
+    if population[0].fitness <= bestValue:
+        print ("세대 번호= ", count)
+        print_p (population)
+
     count += 1
-    if count > 50000: break
+    if count > 1000:
+        break
